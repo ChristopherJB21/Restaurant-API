1cf4cb032072824f0d3e6e7af427efd111451d41
CHRISTOPHER GAVRA RESWARA <christopher.reswara@binus.edu>
2024-08-12 22:07:49 +0700

    Pasang kembali JWT

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,41 +3,43 @@ package middleware
 import (
 	"crypto/rsa"
 	"net/http"
+	"restaurant/helper"
+	"restaurant/model/web"
 
 	"github.com/go-playground/validator/v10"
 )
 
 type Middleware struct {
-	Handler  http.Handler
-	Validate *validator.Validate
+	Handler      http.Handler
+	Validate     *validator.Validate
 	RSAPublicKey *rsa.PublicKey
 }
 
 func NewMiddleware(handler http.Handler, validate *validator.Validate, rSAPublicKey *rsa.PublicKey) *Middleware {
 	return &Middleware{
-		Handler:  handler,
-		Validate: validate,
+		Handler:      handler,
+		Validate:     validate,
 		RSAPublicKey: rSAPublicKey,
 	}
 }
 
 func (middleware *Middleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	// err := helper.VerifyToken(request, middleware.RSAPublicKey)
+	err := helper.VerifyToken(request, middleware.RSAPublicKey)
 
-	// if err != nil {
-	// 	writer.Header().Add("Content-Type", "application/json")
-	// 	writer.WriteHeader(http.StatusUnauthorized)
+	if err != nil {
+		writer.Header().Add("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusUnauthorized)
 
-	// 	webResponse := web.WebResponse{
-	// 		Code:   http.StatusUnauthorized,
-	// 		Status: "UNAUTHORIZED",
-	// 		Data: err.Error(),
-	// 	}
+		webResponse := web.WebResponse{
+			Code:   http.StatusUnauthorized,
+			Status: "UNAUTHORIZED",
+			Data:   err.Error(),
+		}
 
-	// 	helper.WriteToResponseBody(writer, webResponse)
+		helper.WriteToResponseBody(writer, webResponse)
 
-	// 	return
-	// }
+		return
+	}
 
 	middleware.Handler.ServeHTTP(writer, request)
 }
